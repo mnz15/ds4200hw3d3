f47d4398f059d0822484c74a2cda15cb7dbdace5
mnz15 <63454203+mnz15@users.noreply.github.com>
2024-12-16 09:04:55 -0500

    Create scatterplot.js

---
+++ scatterplot.js
@@ -0,0 +1,83 @@
+// Set dimensions and margins
+const margin = { top: 20, right: 30, bottom: 50, left: 50 },
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// Create SVG canvas
+const svg = d3.select("#scatterplot")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Load and process the CSV data
+d3.csv("iris.csv").then(data => {
+    // Convert Petal Length and Petal Width to numeric values
+    data.forEach(d => {
+        d.PetalLength = +d['petal_length'];
+        d.PetalWidth = +d['petal_width'];
+    });
+
+    // Define scales
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.PetalLength)])
+        .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.PetalWidth)])
+        .range([height, 0]);
+
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+    // Add X axis
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(xScale))
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", 40)
+        .attr("fill", "black")
+        .text("Petal Length");
+
+    // Add Y axis
+    svg.append("g")
+        .call(d3.axisLeft(yScale))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -40)
+        .attr("fill", "black")
+        .text("Petal Width");
+
+    // Add circles for data points
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.PetalLength))
+        .attr("cy", d => yScale(d.PetalWidth))
+        .attr("r", 5)
+        .attr("fill", d => colorScale(d['species']));
+
+    // Add legend
+    const legend = svg.selectAll(".legend")
+        .data(colorScale.domain())
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("transform", (d, i) => `translate(0, ${i * 20})`);
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .attr("fill", colorScale);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(d => d);
+});
